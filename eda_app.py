28a903e9d92038b7290bf9f372d046463a01caed
onwood <onwood93@gmail.com>
2021-03-11 11:56:51 +0900

    add

--- eda_app.py
+++ eda_app.py
@@ -9,17 +9,21 @@ from sklearn.preprocessing import MinMaxScaler
 
 def run_eda_app():
     df = pd.read_csv('data/Car_Purchasing_Data.csv', encoding='ISO-8859-1')
-    X = df.iloc[ : , 3:-2+1]
+    # X = df.iloc[ : , 3:-2+1]
     
     st.title('사용된 데이터 확인')
     st.write('이 앱은 고객 데이터와 자동차 구매액에 대한 내용을 담고 있습니다. 특정 정보를 입력하면 차량을 구매할 예상액을 도출해낼 수 있는 앱입니다.')
-    columns = X.columns
+    columns = df.columns
+    # print(columns)
+    # print(df.dtypes != object)
 
     st.write('\n')
     st.write('\n')
 
-    multiselected_colums = st.multiselect('컬럼을 선택하세요',columns)
-    
+    number_columns = df.columns[df.dtypes != object]
+    multiselected_colums = st.multiselect('컬럼을 선택하세요',number_columns)
+    # st.write((df.loc(df['Age'] == df['Age'].max()).to_frame(),))
+
     if multiselected_colums != []:
         st.dataframe(df[multiselected_colums])    
         
@@ -29,7 +33,49 @@ def run_eda_app():
         radio_menu = ['통계치','상관 관계']
         selected_radio = st.radio('선택한 데이터의 가공된 데이터를 확인해보세요', radio_menu)
         
-        if selected_radio == '상관 관계':
-            st.dataframe(df[multiselected_colums].corr())
         if selected_radio == '통계치':
             st.dataframe(df[multiselected_colums].describe())
+
+        if selected_radio == '상관 관계':
+            if len(multiselected_colums) > 1:
+                st.dataframe(df[multiselected_colums].corr())
+                
+                fig = sns.pairplot(data = df[multiselected_colums])
+                st.pyplot(fig)
+            
+            else:
+                st.write('두 가지 이상의 데이터를 선택해주세요')
+    
+    else:
+        st.write('선택한 컬럼이 없습니다.')
+
+    # 컬럼 하나 선택하면 해당 컬럼의 min과 max에 해당하는 사람의 데이터 출력
+    st.write('\n')
+    st.write('\n')
+    st.write('\n')
+
+    st.subheader('원하는 컬럼의 최소값과 최대값 정보를 확인해보세요')
+    number_column = df.columns[df.dtypes != object]    
+    selected_col = st.selectbox('컬럼을 선택하세요', number_column)
+    
+    st.write('선택한 컬럼의 최소값 정보')
+    min_data = df.loc[df[selected_col] == df[selected_col].min(),]
+    st.dataframe(min_data)    
+
+    st.write('선택한 컬럼의 최대값 정보')
+    max_data = df.loc[df[selected_col] == df[selected_col].max(),]
+    st.dataframe(max_data)  
+
+
+    st.write('\n')
+    st.write('\n')
+    st.write('\n')
+
+    st.subheader('고객의 이름으로 정보를 확인 해보세요')
+
+
+    # 고객의 이름을 검색할 수 있는 기능 
+    customer_name = st.text_input('검색하려는 고객의 이름을 입력하세요')
+    name_filter = df.loc[df['Customer Name'].str.contains(customer_name, case = False) == True, ]
+    st.dataframe(name_filter)
+
--- ml_app.py
+++ ml_app.py
@@ -13,31 +13,31 @@ def run_ml_app():
     st.title('차량 구매액 예측')
     st.write('성별(Gender), 연령(Age), 연봉(Annual Salary), 카드빚(Credit Card Debt), 순자산(Net Worth)을 직접 입력해서 차량 구매액을 예측해보세요.')
     
-    df = pd.read_csv('data/Car_Purchasing_Data.csv', encoding='ISO-8859-1')
-    X = df.iloc[ : , 3:-2+1]
-    y = df['Car Purchase Amount']
-    
-    model = tf.keras.models.load_model('data/by-type-ANN-onwood-original.h5')
-    sc_X = joblib.load('data/sc_X.pkl')
-    sc_y = joblib.load('data/sc_y.pkl')
+
     
     # gender 선택 / age /  annual salary / credit card debt / net worth
     
-    gender = st.radio('Gender',['Female','Male'])
+    gender = st.radio('성별(Gender)',['Female','Male'])
     if gender == 'Female':
         gender = 0
     else:
         gender = 1        
 
-    age = st.number_input('Age', 1)
-    salary = st.number_input('Annual Salary', 0.0)
-    debt = st.number_input('Credit Card Debt', 0.0)
-    net_worth = st.number_input('Net Worth', 0.0)
+    age = st.number_input('연령(Age)', 0)
+    salary = st.number_input('연봉(Annual Salary)', 0.0)
+    debt = st.number_input('카드빚(Credit Card Debt)', 0.0)
+    net_worth = st.number_input('순자산(Net Worth)', 0.0)
 
-    new_data = np.array([gender, age, salary, debt, net_worth]).reshape(1,-1)  
+    input_data_list=[gender, age, salary, debt, net_worth]
+
+    model = tf.keras.models.load_model('data/by-type-ANN-onwood-original.h5')
+    sc_X = joblib.load('data/sc_X.pkl')
+    sc_y = joblib.load('data/sc_y.pkl')
+
+    new_data = np.array(input_data_list).reshape(1,-1)  
     
     new_data = sc_X.transform(new_data)
     new_data_pred = model.predict(new_data)
-
-    predicted_data = sc_y.inverse_transform(new_data_pred)
-    st.write('예측 값은 {}입니다.'.format(predicted_data))
+    predicted_data_final = sc_y.inverse_transform(new_data_pred)
+    if st.button('결과'):
+        st.write('예측 값은 {:,.2f}입니다.'.format(predicted_data_final[0,0]))
